5aa1fc387dc11bf32a0dd6eafe6c013f12605133
wertmenschen <sebastian.schoeps@gmx.de>
2018-07-11 01:29:57 +0200

    Add auth.

--- config/camunda.php
+++ config/camunda.php
@@ -4,5 +4,9 @@ return [
     'api' => [
         'url' => env('CAMUNDA_API_URL', 'http://localhost:8080/engine-rest'),
         'tenant-id' => env('CAMUNDA_API_TENANT_ID'),
+        'auth' => [
+            'user' => env('CAMUNDA_API_USER'),
+            'password' => env('CAMUNDA_API_PASSWORD'),
+        ],
     ]
 ];
