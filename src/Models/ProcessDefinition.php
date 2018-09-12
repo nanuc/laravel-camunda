2826bd8f1608baa2d7372302025bee09f33e7458
wertmenschen <sebastian.schoeps@gmx.de>
2018-09-12 13:49:31 +0200

    Update ProcessDefinition.php

--- src/Models/ProcessDefinition.php
+++ src/Models/ProcessDefinition.php
@@ -2,22 +2,23 @@
 
 namespace Wertmenschen\CamundaApi\Models;
 
-
 class ProcessDefinition extends CamundaModel
 {
-    protected $key;
-
-    public function startInstance()
-    {
-        $tenant = strlen(config('camunda.api.tenant-id')) ? '/tenant-id/' . config('camunda.api.tenant-id') : '';
-        $processDefinition = $this->post('process-definition/key/' . $this->key . $tenant . '/start');
-        return new ProcessInstance($processDefinition->id);
-    }
-
     public static function byKey($key)
     {
         $processDefinition = new self;
         $processDefinition->key = $key;
         return $processDefinition;
+    }
+    
+    public function startInstance()
+    {
+        $processDefinition = $this->post('start');
+        return new ProcessInstance($processDefinition->id);
+    }
+    
+    public function xml()
+    {
+        return $this->get('xml')->bpmn20Xml;
     }
 }
